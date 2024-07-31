c9d9ddd8cc87473bbaad4182b30a30d9d846ceba
Tyler Harris <tharris@assyst.net>
2024-07-31 10:46:01 -0400

    Updated Repo

---
+++ client/src/app/award-search/award-search/award-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-award-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './award-search.component.html',
+  styleUrl: './award-search.component.scss'
+})
+export class AwardSearchComponent {
+
+}
