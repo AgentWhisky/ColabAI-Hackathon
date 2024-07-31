24594cc1dd49f91b37d62bee46186013e59a0f69
Tyler Harris <tharris@assyst.net>
2024-07-31 12:09:14 -0400

    Setup Initial Search Screen

--- client/src/app/award-search/award-search/award-search.component.ts
+++ client/src/app/award-search/award-search/award-search.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-award-search',
+  selector: "app-award-search",
   standalone: true,
-  templateUrl: './award-search.component.html',
-  styleUrl: './award-search.component.scss',
-  imports: [MatButtonModule],
+  templateUrl: "./award-search.component.html",
+  styleUrl: "./award-search.component.scss",
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule],
 })
 export class AwardSearchComponent {
 
+  sendPrompt(prompt: string) {
+    console.log(prompt);
+    
+  }
 }
