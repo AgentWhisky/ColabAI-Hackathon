99e1603476a98e47a089391d4f0b48b57a45b1ca
Tyler Harris <tharris@assyst.net>
2024-07-31 10:56:52 -0400

    Updated Routes to Redirect to awardsearch on empty

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/awardsearch', pathMatch: 'full' },
     {
         path: 'awardsearch',
         loadComponent: () => import('./award-search/award-search/award-search.component').then((c) => c.AwardSearchComponent),
